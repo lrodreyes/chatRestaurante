645fb5af30514ed7b6743eaeff533c28e47d0a54
rodrigo <rodrigoreyes658@gmail.com>
2017-05-15 11:19:46 -0500

    conexion con firebase

---
+++ src/environments/firebase.conf.ts
@@ -0,0 +1,8 @@
+export const firebaseConf = {
+	apiKey: "AIzaSyCoP1JuA4KkvgtJPx5II1j9C2vV9sbak8o",
+    authDomain: "menu-restaurante.firebaseapp.com",
+    databaseURL: "https://menu-restaurante.firebaseio.com",
+    projectId: "menu-restaurante",
+    storageBucket: "menu-restaurante.appspot.com",
+    messagingSenderId: "327373634763"
+}
