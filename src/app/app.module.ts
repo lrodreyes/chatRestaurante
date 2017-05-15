f9ccfa978930dc6ddeecf2cdaa2bb9f3295da7ef
rodrigo <rodrigoreyes658@gmail.com>
2017-05-15 12:26:54 -0500

    se agrego materialize y jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,24 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { firebaseConf } from '../environments/firebase.conf';
 
+//RUTAS
+import { APP_ROUTING } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/share/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    APP_ROUTING,
     AngularFireModule.initializeApp(firebaseConf),
     AngularFireDatabaseModule
   ],
