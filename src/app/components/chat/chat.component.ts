1b986a420c26288797bdd3d5639b10612a438ef2
CMarianGV <cruzmariangv@gmail.com>
2017-05-18 09:14:47 -0500

    Comentarios listos

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -6,59 +6,59 @@ import { LoginService } from '../../services/login.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
+/*En esta parte estamos utilizando el componente llamado chat para poder utilizar el javascript*/
 export class ChatComponent implements OnInit {
+  /*Aqui vamos a declarar las variables que vamos a utilizar*/
 	mensaje:string="";
 	elemento:any;
   uid='';
   chats;
-
+//Por medio de un constructor hacemos referencia al componente service.ts
   constructor(public _ls:LoginService) {
     if(this._ls.user!="administrador"){
-
+    /*Se hace un llamado a la función cargar mensajes que se encuentra en el ts llamado service que
+    nos sirve para cargar los mensajes*/
      this._ls.cargarMensajes().subscribe( ()=>{
         console.log("Mensajes cargados....");
         setTimeout( ()=>this.elemento.scrollTop=this.elemento.scrollHeight, 50);
       });
     }else if(this._ls.user="administrador"){
+      /*Se llama a la función cargarChats que esta en service para poder visualizar todo las personas
+      que se han logeado y dejado un mensaje*/
       this.chats=this._ls.cargarChats().subscribe( ()=>{
         console.log("Chats cargados...");
         console.log(this.chats);
       });
-
     }
   }
-
   ngOnInit() {
+    /*Estamos mostrando los mensajes en el div llamado app-mensajes*/
   	this.elemento = document.getElementById("app-mensajes");
   }
-
   enviar(){
   	if(this.mensaje.length == 0){
   		//SI NO HAY NADA ESCRITO EN EL INPUT
   		return;
   	}
-
+    /*Envia el mensaje que se localiza en el input del html*/
     this._ls.agregarMensaje(this.mensaje);
+    //limpia el input y la variable mensaje para que pueda escribir un nuevo mensaje
   	this.mensaje="";
   }
-
   enviarAdm(){
     if(this.mensaje.length == 0){
       return;
     }
-
+    //Envia el mensaje junto con el uid (identificador propio de cada usuario) a la función agregarMensajeAdm
     this._ls.agregarMensajeAdm(this.mensaje,this.uid);
     this.mensaje="";
   }
-
   consultar(uid){
-    // console.log("este es el uid del usuario "+uid);
-    // this._ls.cargarMsjUid(uid);
     this.uid=uid;
-    // console.log("uid enviado"+this.uid);
+    /*Carga los mensajes y los envia a la función cargarMsjUid pero ahora personalizadamente,
+    dependiendo de el usuario que se haya seleccionado*/
     this._ls.cargarMsjUid(this.uid).subscribe( ()=>{
         setTimeout( ()=>this.elemento.scrollTop=this.elemento.scrollHeight, 50);
       });
   }
-
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -7,24 +7,23 @@ import * as firebase from 'firebase/app';
 @Injectable()
 export class LoginService {
   user: any;
-  mensajes; 
-  chats;//ALMACENA LAS CONVERSACIONES CON LOS DIFERENTES USUARIOS
-  // comentarios: FirebaseListObservable<any[]>;
+  mensajes;
+  chats;
+  //ALMACENA LAS CONVERSACIONES CON LOS DIFERENTES USUARIOS
 
   constructor(public db: AngularFireDatabase,
           public afAuth: AngularFireAuth) {
- 
+
           //SE CHECA SI HAY UN USUARIO LOGEADO EN LOCALSTORAGE
           if( localStorage.getItem('user')){
-
-              if(localStorage.getItem('user')!='administrador'){//MIENTRAS SEA DIFERENTE DE ADMINISTRADOR
+              //MIENTRAS SEA DIFERENTE DE ADMINISTRADOR
+              if(localStorage.getItem('user')!='administrador'){
               //SE OBTIENE DE LOCALSTORAGE Y SE ALMACENA EN LA VARIABLE USER
               this.user = JSON.parse(localStorage.getItem('user'));
               //SE ESTABLECE LA RUTA EN LA CUAL ESTARAN GUARDADOS LOS MENSAJES DEL USUARIO
               this.mensajes = db.list('/chats/'+this.user.user.uid);
               }else{
                 this.user = localStorage.getItem('user');
-                //AQUI ESTABLECER LA OTRA RUTA
               }
             }else{
               this.user = null;
@@ -32,77 +31,90 @@ export class LoginService {
 
             console.log(this.user);
   }
-  
+
   // AUTENTICACION CON GOOGLE
   login() {
+    /*con esta linea de codigo estamos indicando que nos aparezca en un popUp la ventana en la
+    cual podremos hacer el logeo con GOOGLE*/
     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider(),)
       .then(result=>{
-
+        /*con esta linea de codigo estamos indicando que cuando un usuario ya haya registrado
+        una visita al sitio, pueda guardarse su nombre de usuaurio y su key para poder mostrar
+        el chat que ha tenido*/
         this.db.list('/usuarios').update(result.user.uid,{
           uid:result.user.uid,
           nombre:result.user.displayName
         });
-       // var token = result.credential.accessToken;
-       // The signed-in user info.
-       // this.user = result;
-
-       // console.log(result);
        this.user=result;
        //UNA VEZ QUE EL USUARIO SE AUTENTICA Y SE OBTIENEN SUS DATOS SE ALMACENAN EN LOCALSTORAGE
        localStorage.setItem('user', JSON.stringify(result) );
 
-      });  
-  
-   }
+      });
 
+   }
+/*función del boton del logout*/
   logout() {
+    /*Aqui estamos indicando que por medio de la variable localStorage y la propiedad removeItem
+    lo que contenga user, en este caso el nombre del usuario, pasara a null*/
     localStorage.removeItem('user');
     this.user = null;
+    /*con la funcion signOut estamos cerrando la sesión del usuario y nos mandara un mensaje en la
+    consola que nos indique que la sesión fue cerrada exitosamente */
     this.afAuth.auth.signOut().then(function(){
       console.log("sign-out successful");
     });
   }
 
   agregarMensaje(text:string){
+    /*con la variable mensajes que arriba inicializamos en el constructor e indicamos la ruta donde
+    la información de esa variable estara almacenada en la base de datos, ahora estamos indicando la
+    estructura que va a tener en los archivos JSON cada mensaje*/
     let mensaje={
       nombre:  this.user.user.displayName,
       mensaje:text
     }
-
-    // console.log(mensaje);
+    //Aqui estamos agregando el mensaje a la base de datos
     this.mensajes.push(mensaje);
   }
 
   agregarMensajeAdm(text:string, uid){
+    /*En esta linea estamos indicando el formato que va a tener el documento JSON al momento de guardar
+    el mensaje del administrador hacia el usurio*/
     let mensaje={
       nombre: 'administrador',
       mensaje:text
     }
-
-   this.db.list('/chats/'+uid).push(mensaje);
+    /*En esta linea de código estamos indicando la ruta donde se almacenara el mensaje y despues con el
+    metodo push lo estamos almacenando en firebase (En este caso, estamos indicando la ruta donde el
+    mensaje personalizado debe de ir ya que cada documento JSON es un usurio y le indicamos en que
+    documento JSON se tiene que almacenar dependiendo del usuario al cuál esta contestando)*/
+    this.db.list('/chats/'+uid).push(mensaje);
   }
 
+  /*Con esta función estamos mostrando en pantalla los mensajes que el usuario ha enviado y de igual
+  forma los mensajes que el administrador le ha enviado*/
   cargarMensajes(){
     this.mensajes = this.db.list('/chats/'+this.user.user.uid,{
       query: {
+        //Se ordenan por clave para que los mensajes mas recientes aparezcan al ultimo
         orderByKey:true
       }
     });
-
-    console.log(this.mensajes);
     return this.mensajes;
   }
-
+/*Con esta función estas mostrando individualmente cada mensaje que le ha enviado cada usuario al
+administrador y de igual forma los que el administrador le ha enviado al usuario*/
   cargarMsjUid(uid){
     this.mensajes = this.db.list('/chats/'+uid,{
       query: {
+        //Se ordenan por clave para que los mensajes mas recientes aparezcan al ultimo
         orderByKey:true
       }
     });
-    console.log(this.mensajes);
     return this.mensajes;
   }
-
+/*Con esta funcion, estamos cargando todos las personas que han dejado mensajes para que los pueda
+visualizar el administrador individualmente*/
   cargarChats(){
     this.chats = this.db.list('/usuarios/',{
       query:{
@@ -112,21 +124,17 @@ export class LoginService {
 
     return this.chats;
   }
-
+//Login del administrador
   loginAdministrador(){
     this.logout();
     this.user="administrador"
     localStorage.setItem('user',this.user);
-    console.log(this.user);
   }
-
+//Logout del administrador
   logoutAdministrador(){
     console.log("adm deslogueado");
     localStorage.removeItem('user');
     this.user=null;
-    console.log(this.user);
   }
 
 }
-
-
